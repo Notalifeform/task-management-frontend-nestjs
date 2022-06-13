6529bf2f72d72d379e79150e9aa644d7382997ce
Notalifeform <Notalifeform@users.noreply.github.com>
2022-06-13 16:27:41 +0000

    deploy: f8be10dfad415069e0cf9c1e8868e298de18551c

---
+++ precache-manifest.abe23ac3807cddc2552441d40dab7c61.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f7adece81c63324f3b397ad89b6b318",
+    "url": "https://Notalifeform.github.io/task-management-frontend-nestjs\n/index.html"
+  },
+  {
+    "revision": "72f4db3ba897ae9a8aba",
+    "url": "https://Notalifeform.github.io/task-management-frontend-nestjs\n/static/css/main.11b39f10.chunk.css"
+  },
+  {
+    "revision": "2218588b3d0286220adf",
+    "url": "https://Notalifeform.github.io/task-management-frontend-nestjs\n/static/js/2.0c9e69f2.chunk.js"
+  },
+  {
+    "revision": "72f4db3ba897ae9a8aba",
+    "url": "https://Notalifeform.github.io/task-management-frontend-nestjs\n/static/js/main.ded955a0.chunk.js"
+  },
+  {
+    "revision": "17b408394f6bec30852a",
+    "url": "https://Notalifeform.github.io/task-management-frontend-nestjs\n/static/js/runtime~main.259f2b26.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://arielweinberger.github.io/task-management-frontend/precache-manifest.0a88baa7b4ebb92ea89c64cd3e3f4832.js"
+  "https://Notalifeform.github.io/task-management-frontend-nestjs\n/precache-manifest.abe23ac3807cddc2552441d40dab7c61.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://arielweinberger.github.io/task-management-frontend/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://Notalifeform.github.io/task-management-frontend-nestjs\n/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
